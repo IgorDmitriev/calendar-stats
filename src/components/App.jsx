a30b44be397ffc830d364b2d9a05aa0508ebec60
Igor Dmitriev <igor.s.dmitriev@gmail.com>
2018-01-01 14:24:54 -0800

    Refactor google requests to use http instead of googleapis-node-js package

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,4 @@
 import React, { Component } from 'react';
-import google from 'googleapis';
-
-import { oauth2Client } from '../lib/googleClient';
 import { arrayToObject } from '../lib/helpers';
 
 import Dashboard from './Dashboard';
@@ -9,177 +6,175 @@ import Menu from './Menu';
 
 import './App.css';
 
-const calendar = google.calendar({ version: 'v3', auth: oauth2Client });
-
 class App extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      accessToken: null,
-      events: {},
-      calendars: {},
-    };
+		this.state = {
+			accessToken: null,
+			events: {},
+			calendars: {},
+		};
 
-    this.setAccessToken = this.setAccessToken.bind(this);
-    this.toggleCalendar = this.toggleCalendar.bind(this);
-    this.fetchEvents = this.fetchEvents.bind(this);
-    this.fetchEventsForCalendar = this.fetchEventsForCalendar.bind(this);
-    this.setCalendarFetchError = this.setCalendarFetchError.bind(this);
-  }
+		this.setAccessToken = this.setAccessToken.bind(this);
+		this.toggleCalendar = this.toggleCalendar.bind(this);
+		this.fetchEvents = this.fetchEvents.bind(this);
+		this.fetchEventsForCalendar = this.fetchEventsForCalendar.bind(this);
+		this.setCalendarFetchError = this.setCalendarFetchError.bind(this);
+	}
 
-  setAccessToken(accessToken) {
-    oauth2Client.credentials = {
-      access_token: accessToken,
-    };
+	setAccessToken(accessToken) {
+		this.setState({
+			accessToken: accessToken,
+		});
 
-    this.setState({
-      accessToken: accessToken,
-    });
+		this.fetchCalendars();
+	}
 
-    this.fetchCalendars();
-  }
+	fetchCalendars() {
+		window.gapi.client
+			.request({
+				path:
+					'https://www.googleapis.com/calendar/v3/users/me/calendarList',
+			})
+			.then(response => this.receiveCalendars(response.result.items));
+	}
 
-  fetchCalendars() {
-    calendar.calendarList.list({}, (err, response) =>
-      this.receiveCalendars(response.items),
-    );
-  }
+	fetchEvents() {
+		this.getSelectedCalendarIds().forEach(calendarId =>
+			this.fetchEventsForCalendar(calendarId, ''),
+		);
+	}
 
-  fetchEvents() {
-    this.getSelectedCalendarIds().forEach(calendarId =>
-      this.fetchEventsForCalendar(calendarId, ''),
-    );
-  }
+	fetchEventsForCalendar(calendarId, nextPageToken) {
+		if (nextPageToken === undefined) return;
 
-  fetchEventsForCalendar(calendarId, nextPageToken) {
-    if (nextPageToken === undefined) return;
-    console.log(`fetching calendar ${calendarId}`);
+		window.gapi.client
+			.request({
+				path: `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events`,
+				params: {
+					calendarId,
+					timeMin: new Date('2017-1-1').toISOString(),
+					timeMax: new Date('2018-1-1').toISOString(),
+					showDeleted: false,
+					singleEvents: true,
+					orderBy: 'startTime',
+					pageToken: nextPageToken,
+					maxResults: 2500,
+				},
+			})
+			.then(({ result: { items, nextPageToken } }) => {
+				this.receiveEvents(items, calendarId, nextPageToken);
+			})
+			.catch(response => {
+				console.error(`${calendarId}:`, response);
+				this.setCalendarFetchError(calendarId);
+			});
+	}
 
-    calendar.events.list(
-      {
-        calendarId,
-        timeMin: new Date('2017-1-1').toISOString(),
-        timeMax: new Date('2018-1-1').toISOString(),
-        showDeleted: false,
-        singleEvents: true,
-        orderBy: 'startTime',
-        pageToken: nextPageToken,
-        maxResults: 2500,
-      },
-      (err, response) => {
-        if (err) {
-          this.setCalendarFetchError(calendarId);
-        } else {
-          this.receiveEvents(
-            response.items,
-            calendarId,
-            response.nextPageToken,
-          );
-        }
-      },
-    );
-  }
+	setCalendarFetchError(calendarId) {
+		const { calendars } = this.state;
+		const calendar = calendars[calendarId];
 
-  setCalendarFetchError(calendarId) {
-    const { calendars } = this.state;
-    const calendar = calendars[calendarId];
+		this.setState({
+			calendars: {
+				...calendars,
+				[calendarId]: {
+					...calendar,
+					fetchError: true,
+				},
+			},
+		});
+	}
 
-    this.setState({
-      calendars: {
-        ...calendars,
-        [calendarId]: {
-          ...calendar,
-          fetchError: true,
-        },
-      },
-    });
-  }
+	receiveCalendars(calendars) {
+		this.setState(
+			{ calendars: arrayToObject(calendars) },
+			this.fetchEvents,
+		);
+	}
 
-  receiveCalendars(calendars) {
-    this.setState({ calendars: arrayToObject(calendars) }, this.fetchEvents);
-  }
+	receiveEvents(newEvents, calendarId, nextPageToken) {
+		const { events } = this.state;
+		let eventsForCalendar;
 
-  receiveEvents(newEvents, calendarId, nextPageToken) {
-    const { events } = this.state;
-    let eventsForCalendar;
+		if (events[calendarId] === undefined) {
+			eventsForCalendar = newEvents;
+		} else {
+			eventsForCalendar = [...events[calendarId], ...newEvents];
+		}
 
-    if (events[calendarId] === undefined) {
-      eventsForCalendar = newEvents;
-    } else {
-      eventsForCalendar = [...events[calendarId], ...newEvents];
-    }
+		this.setState(
+			{
+				events: {
+					...events,
+					[calendarId]: eventsForCalendar,
+				},
+			},
+			() => this.fetchEventsForCalendar(calendarId, nextPageToken),
+		);
+	}
 
-    this.setState(
-      {
-        events: {
-          ...events,
-          [calendarId]: eventsForCalendar,
-        },
-      },
-      () => this.fetchEventsForCalendar(calendarId, nextPageToken),
-    );
-  }
+	getSelectedCalendarIds() {
+		const { calendars } = this.state;
 
-  getSelectedCalendarIds() {
-    const { calendars } = this.state;
+		return Object.values(calendars)
+			.filter(({ selected }) => selected)
+			.map(({ id }) => id);
+	}
 
-    return Object.values(calendars)
-      .filter(({ selected }) => selected)
-      .map(({ id }) => id);
-  }
+	getEvents() {
+		const { events } = this.state;
+		const selectedCalendars = this.getSelectedCalendarIds();
+		let selectedEvents = [];
 
-  getEvents() {
-    const { events } = this.state;
-    const selectedCalendars = this.getSelectedCalendarIds();
-    let selectedEvents = [];
+		selectedCalendars.forEach(
+			calendarId =>
+				(selectedEvents = selectedEvents.concat(
+					events[calendarId] || [],
+				)),
+		);
 
-    selectedCalendars.forEach(
-      calendarId =>
-        (selectedEvents = selectedEvents.concat(events[calendarId] || [])),
-    );
+		return selectedEvents;
+	}
 
-    return selectedEvents;
-  }
+	toggleCalendar(calendarId) {
+		const { calendars, events } = this.state;
 
-  toggleCalendar(calendarId) {
-    console.log('toggle', calendarId);
-    const { calendars, events } = this.state;
+		this.setState(
+			{
+				calendars: {
+					...calendars,
+					[calendarId]: {
+						...calendars[calendarId],
+						selected: !Boolean(calendars[calendarId].selected),
+					},
+				},
+			},
+			() => {
+				if (events[calendarId] !== undefined) return;
 
-    this.setState(
-      {
-        calendars: {
-          ...calendars,
-          [calendarId]: {
-            ...calendars[calendarId],
-            selected: !Boolean(calendars[calendarId].selected),
-          },
-        },
-      },
-      () => {
-        if (events[calendarId] !== undefined) return;
+				this.fetchEventsForCalendar(calendarId, '');
+			},
+		);
+	}
 
-        this.fetchEventsForCalendar(calendarId, '');
-      },
-    );
-  }
+	render() {
+		window.state = this.state;
+		const { calendars } = this.state;
 
-  render() {
-    window.state = this.state;
-    const { calendars } = this.state;
-
-    return (
-      <div className="App">
-        <Menu
-          calendars={Object.values(calendars)}
-          selectedCalendars={this.getSelectedCalendarIds()}
-          setAccessToken={this.setAccessToken}
-          toggleCalendar={this.toggleCalendar}
-        />
-        <Dashboard events={this.getEvents()} />
-      </div>
-    );
-  }
+		return (
+			<div className="App">
+				<Menu
+					calendars={Object.values(calendars)}
+					selectedCalendars={this.getSelectedCalendarIds()}
+					setAccessToken={this.setAccessToken}
+					toggleCalendar={this.toggleCalendar}
+				/>
+				<Dashboard events={this.getEvents()} />
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/Menu/Signin/Signin.jsx
+++ src/components/Menu/Signin/Signin.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import GoogleLogin from 'react-google-login';
-import { CLIENT_ID, SCOPES } from '../../../lib/googleClient';
+
+const CLIENT_ID =
+	'118876649158-jjsfa5kaitkgdaag2ai6btunru2ajpcc.apps.googleusercontent.com';
+const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
 
 export const Signin = ({ onLoginSuccess }) => (
-  <GoogleLogin
-    clientId={CLIENT_ID}
-    scope={SCOPES}
-    buttonText="Connect Google Calendar"
-    onSuccess={response => onLoginSuccess(response)}
-    offline={false}
-    approvalPrompt="force"
-  />
+	<GoogleLogin
+		clientId={CLIENT_ID}
+		scope={SCOPES}
+		buttonText="Connect Google Calendar"
+		onSuccess={response => onLoginSuccess(response)}
+		offline={false}
+		approvalPrompt="force"
+	/>
 );
 
 export default Signin;
